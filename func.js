4b037d4e2227daf09c21f0fb8a0130ef7e6311f7
Dimitri Walters <me@dimi.io>
2020-09-09 01:43:09 -0400

    Add abstract func code

--- func.js
+++ func.js
@@ -1,4 +1,5 @@
 // arrow functions
+
 const add = (x,y) => x + y;
 console.log(add(1,2));
 
@@ -6,3 +7,40 @@ const add2 = (x) => (y) => x + y;
 console.log(add2(1)(2));
 addOne = add2(1);
 console.log(addOne(2));
+
+// abstract funcs - map
+
+arr = [1,2,3,4]
+
+arr2 = []
+for (let i=0; i<4; i++) {
+    arr2[i] = arr[i] + 1; // <-- imperactive
+}
+console.log(arr2);
+
+arr3 = arr.map(addOne); // <-- functional
+console.log(arr3);
+
+// abstract funcs - filter
+
+arr4 = []
+for (let i=0; i<4; i++) {
+    if (arr[i] % 2 == 0) {
+        arr4.push(arr[i]);
+    }
+}
+console.log(arr4);
+
+arr5 = arr.filter((x) => x % 2 == 0);
+console.log(arr5);
+
+// abstract funcs - reduce
+
+sum1 = 0
+for (let i=0; i<4; i++) {
+    sum1 += arr[i];
+}
+console.log(sum1);
+
+sum2 = arr.reduce(add);
+console.log(sum2);
